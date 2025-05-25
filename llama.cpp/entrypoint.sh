e573ff708427655477beab4b9bff1eefd961420f
pasha <paul@drteam.rocks>
2025-05-25 14:10:55 +0300

    Custom CMD support

--- llama.cpp/entrypoint.sh
+++ llama.cpp/entrypoint.sh
@@ -2,6 +2,13 @@
 
 cd "$(dirname "$0")"
 
+# If arguments passed to the script — treat them as custom command
+if [ "$#" -gt 0 ]; then
+    echo && echo "Custom CMD detected, executing: $*" && echo
+    exec "$@"
+fi
+
+# Default env setup
 [ "x$APP_MODE" = "x" ] && export APP_MODE="backend"
 [ "x$APP_BIND" = "x" ] && export APP_BIND="0.0.0.0"
 [ "x$APP_MEM" = "x" ] && export APP_MEM="1024"
@@ -9,6 +16,9 @@ cd "$(dirname "$0")"
 [ "x$APP_MODEL" = "x" ] && export APP_MODEL="/app/models/TinyLlama-1.1B-q4_0.gguf"
 [ "x$APP_REPEAT_PENALTY" = "x" ] && export APP_REPEAT_PENALTY="1.0"
 [ "x$APP_GPU_LAYERS" = "x" ] && export APP_GPU_LAYERS="99"
+[ "x$APP_THREADS" = "x" ] && export APP_THREADS="16"
+[ "x$APP_DEVICE" = "x" ] && unset APP_DEVICE
+[ "x$APP_CACHE" = "x" ] && export APP_CACHE="false"
 
 # Construct the command with the options
 if [ "$APP_MODE" = "backend" ]; then
@@ -18,6 +28,9 @@ if [ "$APP_MODE" = "backend" ]; then
     CMD+=" --host $APP_BIND"
     CMD+=" --port $APP_PORT"
     CMD+=" --mem $APP_MEM"
+    CMD+=" --threads $APP_THREADS"
+    [ -n "$APP_DEVICE" ] && CMD+=" --device $APP_DEVICE"
+    [ "$APP_CACHE" = "true" ] && CMD+=" --cache"
 elif [ "$APP_MODE" = "server" ]; then
     [ "x$APP_PORT" = "x" ] && export APP_PORT="8080"
     # API server connected to multipla backends
